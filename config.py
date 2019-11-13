31e3464fa1aa42d4fe72bcfd68e3b44735155a14
Samuel Diaz <sdman135@gmail.com>
2019-11-13 12:57:07 -0500

    updated

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ password = '12345678'
 nfl_key = '9a39917fef3140a6ae071847a5ef02e5'
 mlb_key = '4a30e428a292425480a96cb7b38b9b3b'
 nhl_key = 'd4d52486bd1a49fe9980121976a2f59c'
-nba_key = '3158e62a849246329e3b3185ae4339cb'
+nba_key = '912aa21767msh00835e94c88a76dp127f61jsnacbcfb6be340'
 
 
 nfl_url = 'https://api.sportsdata.io/v3/nfl/scores/json/Scores/2018'
 mlb_url = 'https://api.sportsdata.io/v3/mlb/scores/json/Games/2019'
 nhl_url = 'https://api.sportsdata.io/v3/nhl/scores/json/Games/2019'
-nba_url = ''
+nba_url = 'https://api-nba-v1.p.rapidapi.com/games/seasonYear/2018'
