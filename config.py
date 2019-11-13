e55d3aa7bc153b93fad20f62642245df2e1555f6
sdman135 <53463830+sdman135@users.noreply.github.com>
2019-11-13 10:01:50 -0500

    added new config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+host = 'groupproject-2.cftxbuh9abcx.us-east-1.rds.amazonaws.com'
+user = 'admin'
+password = '12345678'
+nfl_key = '9a39917fef3140a6ae071847a5ef02e5'
+mlb_key = '4a30e428a292425480a96cb7b38b9b3b'
+nhl_key = 'f8406977794d445ca002d757fcfc21af'
+nba_key =
