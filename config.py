1d2199a00d939ef680df9a957e8dc2854702eb9a
Samuel Diaz <sdman135@gmail.com>
2019-11-13 10:06:39 -0500

    fixed an error in config

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ password = '12345678'
 nfl_key = '9a39917fef3140a6ae071847a5ef02e5'
 mlb_key = '4a30e428a292425480a96cb7b38b9b3b'
 nhl_key = 'f8406977794d445ca002d757fcfc21af'
-nba_key =
+nba_key = ''
